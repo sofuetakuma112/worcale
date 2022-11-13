29f7921bc634e77d958f0a1de49da92984c39cdf
sofuetakuma <kawahagi0620@gmail.com>
2022-11-13 23:58:54 +0900

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// import { db } from "./libs/firebase";
+// import { useEffect } from "react";
+
+// import { collection, addDoc } from "firebase/firestore";
 
 function App() {
+  // useEffect(() => {
+  //   const async = async () => {
+  //     const docRef = await addDoc(collection(db, "users"), {
+  //       first: "Ada",
+  //       last: "Lovelace",
+  //       born: 1815,
+  //     });
+  //     console.log("Document written with ID: ", docRef.id);
+  //   };
+  //   async();
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
